e3146a4ed1503be90b48c18e14f7881f7498b89b
Wanda Young <wyoung@sandiego.edu>
2024-10-03 18:09:04 -0700

    correctly fetches user data object and outputs in console, working on extracting display name to show in dashboard

--- app/protected/client/page.tsx
+++ app/protected/client/page.tsx
@@ -1,33 +1,39 @@
-
+'use client'
 import { logoutClick } from "@/app/page";
-import { useSession } from "next-auth/react";
+import { fetchUserData } from "@/app/page";
+import { useEffect, useState } from "react";
+
 const Client = () => {
-   // const { data: session } = useSession();
+    const [userData, setUserData] = useState(null);
+
+    useEffect(()=> {
+        const getData = async () => {
+            const data = await fetchUserData();
+            setUserData(data);
+        };
+        getData();
+    },[]);
+        console.log(userData);
+
 
     return(
-        <div>
-            hi
-            <div>
-            <button onClick={logoutClick}>Log out</button>
-            </div>
-        </div>
-        /*
+        
+        
+        
         <div className="items-center h-screen justify-center text-center text-green-400">
             <div className="mt-10 font-bold text-2xl">
             Welcome to your Spotify Dashboard
             </div>
             <div>
-                <UserInfo/>
             </div>
             <div className="text-white">
 
             </div>
             <div className="justify-right">
-            <Link href="/playlists">See all playlists</Link>
             </div>
-        <button onClick={logOut}>Log out</button>
+        <button onClick={logoutClick}>Log out</button>
         </div>
-*/
+
     )
 }
 
