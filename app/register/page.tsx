adc1ed132e03cf8eab7480c67eb4dd2798ef4e47
Wanda Young <wyoung@sandiego.edu>
2024-10-30 18:24:59 -0700

    aligned elements on registration page, need to redirect back to home page when "go back" button is clicked

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,12 @@ import { signup } from "../actions/auth"
 import { usersTable } from "../db/schema";
 import { db } from "../db";
 
+const goHome = () => {
+    const url = '/'
+    window.location.href = url; // Redirect to home page
+};
+
+
 export default async function Register(){
 
     const post = await db.query.usersTable.findMany();
@@ -16,29 +22,31 @@ export default async function Register(){
                 email: "test@example.com",
                 name: "bob",
             })
-        }} className="flex items-center justify-center min-h-screen gap-5">
+        }} className="flex items-center justify-center min-h-screen ml-5 mr-5 font-semibold">
             <div>
-                <div className="font-bold">Register User</div>
+                <div className="font-bold text-4xl mb-10 text-green-400">Register User</div>
             <div>
-                <label htmlFor="name">Name</label>
-                <input id="name" name="name" placeholder= "Name"/>
+                <label htmlFor="username" className="block mb-1 mt-2">Username</label>
+                <input id="username" name="username" className="block" placeholder= "username"/>
             </div>
             <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" name="email" placeholder= "Email"/>
+                <label htmlFor="email" className="block mb-1 mt-2 ">Email</label>
+                <input id="email" name="email"  className="block" placeholder= "test@example.com"/>
             </div>
             <div>
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password"/>
+                <label htmlFor="password" className="block mb-1 mt-2">Password</label>
+                <input id="password" name="password" className="block" type="password"/>
             </div>
-            <button className="bg-slate-500 gap-5" type="submit">Sign Up</button>
+            <button className="mt- gap-5 justify-center" type="submit">Sign Up</button>
+            <button onClick={goHome}className="mt-5 ml-5">Go Home</button>
             </div>
         </form>
         <div>
+            
+            </div>
         
         </div>
 
-        </div>
 
     )
 };
